da8765278d64a4a81cb454c0c2b21e172656ae4d
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-08-02 18:56:37 +0800

    Update save_obj.m

--- save_obj.m
+++ save_obj.m
@@ -1,12 +1,6 @@
-%将结果输入new_Bunny_head.obj
 function  save_obj(vertex,face,name)
-fid=fopen([name],'w');
-x=1;
-for i=1:size(vertex,1)
-        fprintf(fid,'v %.10f %.10f %.10f\r\n',vertex(i,1),vertex(i,2),vertex(i,3)); 
-end
-for i=1:size(face,1)
-    fprintf(fid,'f %d %d %d\r\n',face(i,1),face(i,2),face(i,3));      
-end
+fid=fopen(name,'wt');
+fprintf(fid,'v %.10f %.10f %.10f\r\n',vertex(:,1),vertex(:,2),vertex(:,3)); 
+fprintf(fid,'f %d %d %d\r\n',face(:,1),face(:,2),face(:,3));      
 fclose(fid);
 end
