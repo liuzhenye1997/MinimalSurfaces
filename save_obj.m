0dc0f5748fbdcca6d7e5870fe61f186cbb2b3d94
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2019-08-02 19:27:14 +0800

    Update save_obj.m

--- save_obj.m
+++ save_obj.m
@@ -1,6 +1,10 @@
 function  save_obj(vertex,face,name)
-fid=fopen(name,'wt');
-fprintf(fid,'v %.10f %.10f %.10f\r\n',vertex(:,1),vertex(:,2),vertex(:,3)); 
-fprintf(fid,'f %d %d %d\r\n',face(:,1),face(:,2),face(:,3));      
+fid=fopen([name],'w');
+size(vertex)
+vertex=vertex';
+fprintf(fid,'v %.10f %.10f %.10f\r\n',vertex(:)); 
+face=face';
+fprintf(fid,'f %d %d %d\r\n',face(:)); 
 fclose(fid);
 end
+
